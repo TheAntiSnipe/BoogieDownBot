4b9463d0671c7d4eb11cd8ef80b136d8b2457164
TheAntiSnipe <38178591+TheAntiSnipe@users.noreply.github.com>
2018-09-18 18:08:49 +0530

    caps lol

--- BoogieDownBot.py
+++ BoogieDownBot.py
@@ -15,7 +15,7 @@ except ImportError:
 from datetime import datetime
 from threading import Timer
 TOKEN = environ['TOKEN']
-PRINT(TOKEN)
+print(TOKEN)
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 def get_url(url):
