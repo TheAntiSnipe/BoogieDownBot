0c4f4481c07f8c5fcf9fe75bca9d264d98ea52bf
TheAntiSnipe <38178591+TheAntiSnipe@users.noreply.github.com>
2019-03-16 09:39:08 +0530

    Add files via upload

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import requests
+import stagetwo
 from bs4 import BeautifulSoup
 
 def download_track(trackNo):
@@ -7,7 +8,8 @@ def download_track(trackNo):
     soup = BeautifulSoup(r.text, 'html.parser')
     songBaseLink= soup.find('div', {'id':'main'}).find('div',{'id':'content_wrapper'}).find('div',{'id':'tracks'}).find_all('div',{'class':'post post-index clearfix'})[trackNo-1].find_all('div',{'class':'clearfix'})[0].find_all('div',{'class':'post-content'})[0].find_all('div',{'class':'track'})[0].find_all('a')[0].get('href')
     songBaseLink=songBaseLink[:-1]
-    songBaseLink+='?st=Tb43un7paxA8Ci9-XNmWzw&e=1539484241?fdl=1'
+    songBaseLink = stagetwo(songBaseLink)
+    #songBaseLink+='?st=Tb43un7paxA8Ci9-XNmWzw&e=1539484241?fdl=1'
     return songBaseLink
 
 if __name__ == '__main__':
