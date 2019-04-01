be2328b0f53cf064a338538331e94efd0a06e536
Mihir Kulkarni <38178591+TheAntiSnipe@users.noreply.github.com>
2019-04-01 17:44:23 +0530

    Add files via upload
    Thumbnail functionality attempt

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ def download_track(trackNo):
     baseURL = f'https://www.clubdancemixes.com/'
     r=requests.get(baseURL)
     soup = BeautifulSoup(r.text, 'html.parser')
-    songBaseLink= soup.find('div', {'id':'main'}).find('div',{'id':'content_wrapper'}).find('div',{'id':'tracks'}).find_all('div',{'class':'post post-index clearfix'})[trackNo-1].find_all('div',{'class':'clearfix'})[0].find_all('div',{'class':'post-content'})[0].find_all('div',{'class':'track'})[0].find_all('a')[0].get('href')
+    songBaseLink= soup.find('div', {'id':'main'}).find('div',{'id':'content_wrapper'}).find('div',{'id':'tracks'}).find_all('div',{'class':'post post-index clearfix'})[trackNo].find_all('div',{'class':'clearfix'})[0].find_all('div',{'class':'post-content'})[0].find_all('div',{'class':'track'})[0].find_all('a')[0].get('href')
     songBaseLink=songBaseLink[:-1]
     songBaseLink = 'https://www.clubdancemixes.com'+  stagetwo.stagetwo(songBaseLink)
     #songBaseLink+='?st=Tb43un7paxA8Ci9-XNmWzw&e=1539484241?fdl=1'
---
+++ thumbnail_scrape.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getThumbnail(tracks):
+    baseURL = f'https://www.clubdancemixes.com/'
+    r=requests.get(baseURL)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    thumbnail = soup.find('div', {'id':'main'}).find('div',{'id':'content_wrapper'}).find('div',{'id':'tracks'}).find_all('div',{'class':'post post-index clearfix'})[tracks].find_all('div',{'class':'clearfix'})[0].find_all('div',{'class':'featured-image-box'})[0].find_all('a')[0].find_all('img')[0].get('src')
+    return thumbnail
+
+if __name__ == '__main__':
+ 	print(getThumbnail(1)) 
