74bd4a596f070e3e07a02427d5959584871beb75
Mihir Kulkarni <38178591+TheAntiSnipe@users.noreply.github.com>
2020-04-02 11:36:20 +0530

    Reverted to September 2019

--- webscrap.py
+++ webscrap.py
@@ -1,21 +1,13 @@
 import requests
-from stagetwo import stagetwo
 from bs4 import BeautifulSoup
-
-
-def song_scrape(baseURL = "https://www.clubdancemixes.com/"):
+def song_scrape(baseURL = f'https://www.clubdancemixes.com/'):
     r=requests.get(baseURL)
     soup = BeautifulSoup(r.text, 'html.parser')
-    songs = {}
+    songNames=""
     track=0
     while track<5:
-        post_content = soup.find('div', {'id':'main'}).find('div',{'id':'content_wrapper'}).find('div',{'id':'tracks'}).find_all('div',{'class':'post post-index clearfix'})[track].find_all('div',{'class':'clearfix'})[0].find_all('div',{'class':'post-content'})[0]
-        track_title = post_content.find_all('h2',{'class':'post-title'})[0].text.strip()
-        track_link = 'https://www.clubdancemixes.com'+stagetwo(post_content.find_all('div',{'class':'track'})[0].find_all('a')[0].get('href'))
-        songs[track_title] = track_link
+        songNames = songNames+ (soup.find('div', {'id':'main'}).find('div',{'id':'content_wrapper'}).find('div',{'id':'tracks'}).find_all('div',{'class':'post post-index clearfix'})[track].find_all('div',{'class':'clearfix'})[0].find_all('div',{'class':'post-content'})[0].find_all('h2',{'class':'post-title'})[0].text).strip()+'/download'+str(track+1)+"\n"
         track+=1
-    return songs
-
-
+    return songNames
 if __name__=="__main__":
     print(song_scrape())
